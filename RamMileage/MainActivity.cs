236368292133dbd1e0645bc2aea785cd18c90660
Devmolasses <devmolasses@gmail.com>
2016-08-25 10:31:23 -0500

    First commit

---
+++ RamMileage/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace RamMileage
+{
+	[Activity(Label = "RamMileage", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			EditText distanceText = FindViewById<EditText>(Resource.Id.DistanceText);
+			EditText mileageText = FindViewById<EditText>(Resource.Id.MileageText);
+			EditText timeText = FindViewById<EditText>(Resource.Id.TimeText);
+			EditText fuelText = FindViewById<EditText>(Resource.Id.FuelText);
+			Button postButton = FindViewById<Button>(Resource.Id.PostButton);
+
+			string stringToPost = string.Empty;
+
+			postButton.Click += (object sender, EventArgs e) =>
+			{
+				//Create the https string to post the data
+				stringToPost = RamMileage.MileagePostString.ToPostString(distanceText.Text, mileageText.Text, timeText.Text, fuelText.Text);
+
+				//Post the string to Thingspeak
+
+			};
+
+		}
+	}
+}
+
+
---
+++ RamMileage/MileagePostString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+namespace RamMileage
+{
+	public static class MileagePostString
+	{
+		public static string ToPostString(string distance, string mileage, string time, string fuel)
+		{
+			//string channelID = "148709";
+			string writeKey = "VTY04EOQ2CDVSKDQ";
+
+			return string.Format("https://api.thingspeak.com/update.json?api_key={0}&field1={1}&field2={2}&field3={3}&field4={4}", writeKey, distance, mileage, time, fuel);
+		}
+	}
+}
+
